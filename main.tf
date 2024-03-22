7380930b5511bdb61317ef31477436b51418e42e
Yeji Kim <125236337+yejimz@users.noreply.github.com>
2024-03-22 16:49:23 +0900

    Update

---
+++ main.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "example" {
+  # vpc_id = aws_vpc.vpc-test-stn.id
+  name   = "example"
+  description = "Example security group for EC2 instances"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    
+    ## 보안 취약점: 인터넷에 대한 액세스를 제어하지 않음
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all inbound traffic"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+}
