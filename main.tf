b917a453fcf311417d492a6d192e1dd75830d6d1
Yeji Kim <125236337+yejimz@users.noreply.github.com>
2024-03-26 15:54:11 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,13 @@
 resource "aws_security_group" "drift-detection" {
   # vpc_id = aws_vpc.vpc-test-stn.id
   name   = "drift-detection-sg"
-  description = "Example security group for EC2 instances"
+  description = "Security group for testing terraform enterprise drift detection"
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     
-    ## 보안 취약점: 인터넷에 대한 액세스를 제어하지 않음
     cidr_blocks = ["0.0.0.0/0"]
     description = "Allow all inbound traffic"
   }
